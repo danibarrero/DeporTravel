49354061f1efa07b9a5f6c7ea9ad67d98c70bb67
Daniel <danielbarrerosojo@gmail.com>
2025-05-28 17:21:15 +0200

    Arreglar login :(

--- frontend/DeporTravel/src/app/components/shared/header/header.component.ts
+++ frontend/DeporTravel/src/app/components/shared/header/header.component.ts
@@ -1,31 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, RouterLink, RouterModule } from '@angular/router';
-import { StorageService } from '../../../services/storge.service';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, CommonModule, RouterModule],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+  styleUrl: './header.component.css'
 })
-export class HeaderComponent implements OnInit {
-  isLoggedIn = false;
-  username = '';
+export class HeaderComponent {
 
-  constructor(
-    private storageService: StorageService,
-    private router: Router
-  ) {}
-
-  ngOnInit(): void {
-    this.isLoggedIn = this.storageService.isLoggedIn();
-    const user = this.storageService.getUser();
-    this.username = user.nombre || user.correoElectronico;
-  }
-
-  cerrarSesion(): void {
-    this.storageService.clean();
-    this.router.navigate(['/login']).then(() => window.location.reload());
-  }
 }
